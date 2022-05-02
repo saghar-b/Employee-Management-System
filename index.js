441712081c379fc65f091030f0b9765b49ef7d50
saghar <behinaeen.saghar@gmail.com>
2022-05-01 22:46:32 -0700

    add role

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const queries = require('./queries/queries.js');
 const logo = require('./asset/logo.js')
 const { error } = require("console");
 const { start } = require("repl");
+const { all } = require("async");
 
 function intro() {
     console.log(logo.printLogo());
@@ -14,38 +15,82 @@ function intro() {
     init();
 }
 const init = async () => {
-    try {
-       
+    var allDep;
+    // try {
 
-        let selectMenue = await ShowMenu();
-        console.log(selectMenue.menu);
-        switch (selectMenue.menu) {
-            case 'View all Departments':
-                const allDep = await queries.viewAllDepartments();
-                console.table(allDep)
-                await init();
-                break;
-            case 'View all manager Roles':
-                const allRoles = await queries.viewAllRols();
-                console.table(allRoles)
-                init();
-                break
-            case 'view all Employees':
-                const allEmp = await queries.viewAllEmployees();
-                console.table(allEmp)
-                init();
-                break;
 
-            case 'Quit':
-                console.log("bye")
-                break;
+    let selectMenue = await ShowMenu();
+    // console.log(selectMenue.menu);
+    switch (selectMenue.menu) {
+        case 'View all Departments':
+            allDep = await queries.viewAllDepartments();
+            console.table(allDep)
+            await init();
+            break;
+        case 'View all manager Roles':
+            const allRoles = await queries.viewAllRols();
+            console.table(allRoles)
+            init();
+            break
+        case 'view all Employees':
+            const allEmp = await queries.viewAllEmployees();
+            console.table(allEmp)
+            init();
+            break;
 
-        }
-    } catch {
-        throw error;
+        case 'Add Role':
+            allDep = await queries.viewAllDepartments();
+            console.log(allDep)
+            let new_role = await showDepartments(allDep)
+            const dep=allDep.filter(i =>{
+               return i.name === new_role.dep
+            })
+            console.log("-----")
+            // console.log(new_role.dep)
+            // console.log(allDep)
+            // console.log(dep)
+            // console.log(dep[0].id)
+            let newRole= new role(new_role.title, new_role.salary , dep[0].id)
+        //    console.log(newRole)
+            await queries.addNewRole(newRole)
+            break;
+
+        case 'Quit':
+            console.log("bye")
+            break;
+
     }
+    // } catch {
+    //     throw error;
+    // }
 }
+const showDepartments = (departments) => {
+    const depName = []
+    const depId = []
+    departments.forEach(element => {
+        depName.push(element.name)
+        depId.push(element.id)
+    });
 
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the role?',
+        }, {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of this role?',
+        },
+        {
+            type: 'rawlist',
+            name: 'dep',
+            message: 'Which department this role belong to?',
+            choices: depName
+        }
+    ])
+
+}
 const ShowMenu = () => {
 
     return inquirer.prompt([
@@ -55,6 +100,7 @@ const ShowMenu = () => {
             message: 'What would you like to do?',
             choices: ['View all Departments', 'View all manager Roles', 'view all Employees', 'Add Role', 'Add Department', 'Quit']
         }
+
     ])
 }
 intro()
--- queries/queries.js
+++ queries/queries.js
@@ -14,25 +14,7 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the Emp_Man_db database.`)
 );
-// db.query = util.promisify(db.query).bind(db);
-// db.connect(function(err){
-//     if (err) {
-//         console.log("error connecting: " + err.stack);
-//         return;
-//     };
-//     console.log("connected as... " + db.threadId);
-// });
 
-// getUser: async (req, res) => {
-//     let queryString = `SELECT * from acct WHERE ACCT_KEY = 89`;  
-//     const [user] = await db.query(queryString).catch(err => {throw err}); 
-//     res.json(user); 
-//  }
-//  const viewAllDepartments = async(req,res) =>{
-//     const sql="SELECT * FROM department;";
-//     const [user] = await db.query(queryString).catch(err => {throw err}); 
-//     res.json(user); 
-//  }
 async function viewAllDepartments() {
     const sql = "SELECT * FROM department;";
     const [rows, fields] = await db.promise().query(sql);
@@ -41,17 +23,30 @@ async function viewAllDepartments() {
 
 async function viewAllEmployees() {
     const sql = "SELECT * FROM employee;";
-    const [rows,field]= await db.promise().query(sql);
+    const [rows, field] = await db.promise().query(sql);
     return rows
 }
-async function viewAllRols(){
+async function viewAllRols() {
     const sql = "SELECT * FROM role;";
-    const [rows,field]= await db.promise().query(sql);
+    const [rows, field] = await db.promise().query(sql);
     // console.log(field)
     return rows;
+}
+
+async function addNewRole(new_role) {
+
+    const sql = 'INSERT INTO role (title, salary, department_id) VALUES (? , ? , ?)';
+ 
+    // const sql = 'INSERT INTO role (title, salary, department_id) VALUES (?)',["new_role.title",2000,3];
+    const params = [new_role.title,parseFloat(new_role.salary) ,parseInt(new_role.depId)];
+    // const params = ["new_role.title",2000,3];
+    console.log(params);
+    const [rows, field] = await db.promise().query(sql,params);
+
 }
 module.exports = {
     viewAllDepartments,
     viewAllEmployees,
-    viewAllRols
+    viewAllRols,
+    addNewRole
 };
